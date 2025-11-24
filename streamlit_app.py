9d04301a655edeed5d59133ae4f35ad726a5f3ed
zulfihossain03 <zulfihossain03@gmail.com>
2025-11-24 15:16:50 +1300

    Update streamlit_app.py
    indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,9 +37,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-      # # st.text(smoothiefroot_response.json())
-      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        # # st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
