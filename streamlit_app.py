d91aab834f7681ac9c998b91b48cd110bba309f1
zulfihossain03 <zulfihossain03@gmail.com>
2025-11-24 15:16:19 +1300

    Update streamlit_app.py
    update with nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -36,6 +37,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      # # st.text(smoothiefroot_response.json())
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -48,7 +52,6 @@ if ingredients_list:
     if time_to_submit:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-# import requests
-# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# # st.text(smoothiefroot_response.json())
-# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+
